14c56984584055a31eff25b02c80a6230c4ae0c1
Pawel Kiszczak <pawel.kiszczak@icloud.com>
2025-12-10 22:58:01 +0100

    update with Pydantic validation

--- project-2/books.py
+++ project-2/books.py
@@ -1,4 +1,6 @@
-from fastapi import FastAPI, Body
+from fastapi import FastAPI
+from pydantic import BaseModel, Field
+from typing import Optional
 
 app = FastAPI()
 
@@ -15,6 +17,14 @@ class Book:
         self.description = description
         self.author = author
         self.rating = rating
+        
+class BookRequest(BaseModel):
+    id: Optional[int] = None
+    title: str = Field(min_length=3)
+    author: str = Field(min_length=1)
+    description: str = Field(min_length=1, max_length=100)
+    rating: int = Field(gt=0, lt=6)
+
 
 BOOKS = [
     Book(1, "Computer Science Pro", "A must have", "JJ Smith", 5),
@@ -30,5 +40,11 @@ async def read_all_books():
     return BOOKS
 
 @app.post("/create_book")
-async def create_book(new_book=Body()) -> None:
+async def create_book(book_request: BookRequest) -> None:
+    new_book = Book(**book_request.model_dump())
     BOOKS.append(new_book)
+    
+    
+def find_book_id(book: Book) -> Book:
+    book.id = 1 if len(BOOKS) == 0 else BOOKS[-1].id + 1    
+    return book
