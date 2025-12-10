ffb7ab21f80afbe5c30cfdb813005194a9967c26
Pawel Kiszczak <pawel.kiszczak@icloud.com>
2025-12-10 09:55:24 +0100

    update endpoint with dynamic parameter

--- books.py
+++ books.py
@@ -17,3 +17,11 @@ async def first_api_endpoint():
 @app.get("/books")
 async def read_all_books():
     return BOOKS
+
+@app.get("/books/{book_title}")
+async def read_all_books(book_title: str):
+    for book in BOOKS:
+        if book["title"].casefold() == book_title.casefold():
+            return book
+    
+    return {"message": "Book not found"}
