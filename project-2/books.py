ed020b2c9456c2eeb2941529e597f0211e4397e9
Pawel Kiszczak <pawel.kiszczak@icloud.com>
2025-12-10 23:18:16 +0100

    add more PUT & DELETE

--- project-2/books.py
+++ project-2/books.py
@@ -19,11 +19,22 @@ class Book:
         self.rating = rating
         
 class BookRequest(BaseModel):
-    id: Optional[int] = None
+    id: Optional[int] = Field(description='ID is not needed to create', default=None)
     title: str = Field(min_length=3)
     author: str = Field(min_length=1)
     description: str = Field(min_length=1, max_length=100)
     rating: int = Field(gt=0, lt=6)
+    
+    model_config = {
+        "json_schema_extra": {
+            "example": {
+                "title": "A new book",
+                "author": "JK Rowling",
+                "description": "A new description of our book",
+                "rating": 5
+            }
+        }
+    }
 
 
 BOOKS = [
@@ -39,11 +50,38 @@ BOOKS = [
 async def read_all_books():
     return BOOKS
 
+@app.get("/books/{book_id}")
+async def read_book(book_id: int):
+    for book in BOOKS:
+        if book_id == book.id:
+            return book
+        
+@app.get("/books/")
+async def read_book_by_rating(book_rating: int):
+    books_to_return = []
+    for book in BOOKS:
+        if book.rating == book_rating:
+            books_to_return.append(book)
+            
+    return books_to_return
+
 @app.post("/create_book")
 async def create_book(book_request: BookRequest) -> None:
     new_book = Book(**book_request.model_dump())
     BOOKS.append(new_book)
     
+@app.put("/books/update_book")
+async def update_book(book: BookRequest):
+    for i in range(len(BOOKS)):
+        if BOOKS[i].id == book.id:
+            BOOKS[i] = Book(**book.model_dump())
+            
+@app.delete("/books/{book_id}")
+async def delete_book(book_id: int):
+    for i in range(len(BOOKS)):
+        if BOOKS[i].id == book_id:
+            BOOKS.pop(i)
+            break    
     
 def find_book_id(book: Book) -> Book:
     book.id = 1 if len(BOOKS) == 0 else BOOKS[-1].id + 1    
