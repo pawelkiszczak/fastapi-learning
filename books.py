e6de0ba65fea2ec1830571767e7701121588a0e8
Pawel Kiszczak <pawel.kiszczak@icloud.com>
2025-12-10 10:52:59 +0100

    query strings and paths

--- books.py
+++ books.py
@@ -3,11 +3,12 @@ from fastapi import FastAPI
 app = FastAPI()
 
 BOOKS = [
-    {"title": "Title One", "author": "Author One", "category": "Category One"},
-    {"title": "Title Two", "author": "Author Two", "category": "Category Two"},
-    {"title": "Title Three", "author": "Author Three", "category": "Category Three"},
-    {"title": "Title Four", "author": "Author Four", "category": "Category Four"},
-    {"title": "Title Five", "author": "Author Five", "category": "Category Five"},
+    {"title": "Title One", "author": "Author One", "category": "Fiction"},
+    {"title": "Title Two", "author": "Author Two", "category": "Fiction"},
+    {"title": "Title Three", "author": "Author Three", "category": "Fiction"},
+    {"title": "Title Four", "author": "Author Four", "category": "Non-Fiction"},
+    {"title": "Title Five", "author": "Author Five", "category": "Non-Fiction"},
+    {"title": "Title Six", "author": "Author Two", "category": "Non-Fiction"}
 ]
 
 @app.get("/api-endpoint")
@@ -25,3 +26,22 @@ async def read_book_by_title(book_title: str):
             return book
     
     return {"message": "Book not found"}
+
+@app.get("/books/")
+async def read_category_by_query(category: str):
+    books_to_return = []
+    for book in BOOKS:
+        if book["category"].casefold() == category.casefold():
+            books_to_return.append(book)
+    
+    return books_to_return
+
+@app.get("/books/{book_author}/")
+async def read_author_category_by_query(book_author: str, category: str):
+    books_to_return = []
+    for book in BOOKS:
+        if (book["author"].casefold() == book_author.casefold() and \
+            book["category"].casefold() == category.casefold()):
+            books_to_return.append(book)
+    
+    return books_to_return
