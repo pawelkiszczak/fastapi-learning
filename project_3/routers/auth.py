f7c42b2f692f44030839cafa25b41bd13b9578f0
Pawel Kiszczak <pawel.kiszczak@icloud.com>
2025-12-17 10:01:48 +0100

    user creation

--- project_3/routers/auth.py
+++ project_3/routers/auth.py
@@ -1,24 +1,47 @@
-from fastapi import APIRouter
-from models import User
-from request_schemas import CreateUserRequest
+from typing import Annotated, Any, Generator
 
+from database import SessionLocal
+from fastapi import APIRouter, Depends
+from models import User
+from passlib.context import CryptContext
+from request_schemas import CreateUserRequest
+from sqlalchemy.orm import Session
+from starlette import status
+
+# Declare router
 router = APIRouter()
 
+# Declare hashing algo and context
+bcrypt_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 
-@router.post(
-    "/auth",
-)
-async def create_user(create_user_request: CreateUserRequest):
+
+# Get database connection
+def get_db() -> Generator[Session, Any, None]:
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
+
+# Database settings
+DB_DEPENDENCY = Annotated[Session, Depends(get_db)]
+
+
+@router.post("/auth", status_code=status.HTTP_201_CREATED)
+async def create_user(
+    db: DB_DEPENDENCY,
+    create_user_request: CreateUserRequest,
+):
     create_user_model = User(
         email=create_user_request.email,
         username=create_user_request.username,
         first_name=create_user_request.first_name,
         last_name=create_user_request.last_name,
         role=create_user_request.role,
-        hashed_password=create_user_request.password,
+        hashed_password=bcrypt_context.hash(create_user_request.password),
         is_active=True,
     )
 
-    return create_user_model
-
-    return {"user": "authenticated"}
+    db.add(create_user_model)
+    db.commit()
