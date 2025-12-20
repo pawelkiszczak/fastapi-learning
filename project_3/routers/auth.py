60c2f55b24fc383f449bd11c7e06a17a7afa2354
Pawel Kiszczak <pawel.kiszczak@icloud.com>
2025-12-20 20:17:10 +0100

    finish testing

--- project_3/routers/auth.py
+++ project_3/routers/auth.py
@@ -53,7 +53,7 @@ def authenticate_user(username: str, password: str, db):
 def create_access_token(
     username: str, user_id: str, role: str, expires_delta: timedelta
 ):
-    encode = {"sub": username, "id": user_id, "role": role}
+    encode = {"sub": username, "id": user_id, "user_role": role}
     expires = datetime.now(timezone.utc) + expires_delta
     encode.update({"exp": expires})
     return jwt.encode(encode, SECRET_KEY, ALGORITHM)
@@ -64,7 +64,7 @@ async def get_current_user(token: Annotated[str, Depends(oauth2_bearer)]):
         payload = jwt.decode(token, SECRET_KEY, ALGORITHM)
         username: str = payload.get("sub")
         user_id: int = payload.get("id")
-        user_role: str = payload.get("role")
+        user_role: str = payload.get("user_role")
 
         if username is None or user_id is None:
             raise HTTPException(
