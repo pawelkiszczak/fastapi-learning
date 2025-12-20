13f7fcec69cf30dc8285c5902ce44db91ed84635
Pawel Kiszczak <pawel.kiszczak@icloud.com>
2025-12-20 15:37:06 +0100

    finish tests for todo router

--- tests/project_3/test_todos.py
+++ tests/project_3/test_todos.py
@@ -96,3 +96,69 @@ def test_read_one_authenticated_not_found(test_todo):
     response = client.get("/todo/999")
     assert response.status_code == status.HTTP_404_NOT_FOUND
     assert response.json() == {"detail": "Todo not found"}
+
+
+def test_create_todo(test_todo):
+    request_data = {
+        "title": "New Todo!",
+        "description": "New todo description",
+        "priority": 3,
+        "complete": False,
+    }
+
+    response = client.post("/todo/", json=request_data)
+    assert response.status_code == status.HTTP_201_CREATED
+
+    db = TestingSessionLocal()
+    model = db.query(Todo).filter(Todo.id == 2).first()
+    assert model.title == request_data.get("title")
+    assert model.description == request_data.get("description")
+    assert model.complete == request_data.get("complete")
+    assert model.priority == request_data.get("priority")
+
+
+def test_update_todo(test_todo):
+    request_data = {
+        "title": "Change the title",
+        "description": "Need to learn more",
+        "priority": 1,
+        "complete": True,
+    }
+
+    response = client.put("/todo/1", json=request_data)
+    assert response.status_code == status.HTTP_204_NO_CONTENT
+
+    db = TestingSessionLocal()
+    model = db.query(Todo).filter(Todo.id == 1).first()
+    assert model.title == request_data.get("title")
+    assert model.description == request_data.get("description")
+    assert model.complete == request_data.get("complete")
+    assert model.priority == request_data.get("priority")
+
+
+def test_update_todo_not_found(test_todo):
+    request_data = {
+        "title": "Change the title",
+        "description": "Need to learn more",
+        "priority": 1,
+        "complete": True,
+    }
+
+    response = client.put("/todo/999", json=request_data)
+    assert response.status_code == status.HTTP_404_NOT_FOUND
+    assert response.json() == {"detail": "Todo not found"}
+
+
+def test_delete_todo(test_todo):
+    response = client.delete("/todo/1")
+    assert response.status_code == status.HTTP_204_NO_CONTENT
+
+    db = TestingSessionLocal()
+    model = db.query(Todo).filter(Todo.id == 1).first()
+    assert model is None
+
+
+def test_delete_todo_not_found():
+    response = client.delete("/todo/999")
+    assert response.status_code == status.HTTP_404_NOT_FOUND
+    assert response.json() == {"detail": "Todo not found"}
